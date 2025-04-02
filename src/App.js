fa59dbffdd54265b0b95f04d883d74df8aa11d74
Deepika <dipizinbox@gmail.com>
2025-04-02 15:15:25 -0700

    new pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <nav>
-        <Link to="/Home">Home</Link> | <Link to="/About">About</Link>
+        <Link to="/Home">Home</Link> | <Link to="/About">About Me</Link> | <Link to="/Projects">Project Experience</Link> | <Link to="/Contact">Contact Me</Link>
       </nav>
       <Routes>
         <Route path = "/Home" element = {<Home/>}></Route>
         <Route path = "/About" element = {<About/>}></Route>
+        <Route path = "/Projects" element = {<Projects/>}></Route>
+        <Route path = "/Contact" element = {<Contact/>}></Route>
       </Routes>
     </div>
   );
