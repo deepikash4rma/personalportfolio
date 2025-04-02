36b70f35e3ea9229065a1dc9e281a6e880b3cbdd
Deepika <dipizinbox@gmail.com>
2025-04-02 15:01:45 -0700

    no warnings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/Home">Home</Link> | <Link to="/About">About</Link>
+      </nav>
+      <Routes>
+        <Route path = "/Home" element = {<Home/>}></Route>
+        <Route path = "/About" element = {<About/>}></Route>
+      </Routes>
     </div>
   );
 }
